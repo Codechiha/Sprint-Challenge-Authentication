ab77ae15b41ce4824ef1bac4df94929afe59d8bd
David Situ <davidsitu626@gmail.com>
2019-01-25 10:31:04 -0800

    register function complete

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,9 @@
 const axios = require('axios');
 
-const { authenticate } = require('../auth/authenticate');
+const bcrypt = require('bcryptjs');
+const db = require('../database/dbConfig.js')
+
+const { authenticate, jwtKey } = require('../auth/authenticate');
 
 module.exports = server => {
   server.post('/api/register', register);
@@ -10,10 +13,35 @@ module.exports = server => {
 
 function register(req, res) {
   // implement user registration
+  const user = req.body;
+
+  const hash = bcrypt.hashSync(user.password);
+  user.password = hash;
+  db('users')
+    .insert(user)
+    .then(u => {
+      res.status(200).json({ id: u[0]})
+    })
+    .catch(err => res.status(500).json(err))
 }
 
 function login(req, res) {
   // implement user login
+  const userInput = req.body;
+  db('users').where('username', userInput.username)
+  .then(user => {
+      //username valid password from client == password from db
+      if(user && bcrypt.compareSync(userInput.password, user[0].password)){
+
+          res.status(200).json({ message: `welcome`, jwtKey});
+      } else {
+          res.status(404).json({err: 'invalid username or password'})
+      }
+  })
+  .catch(err => {
+      res.status(500).send(err);
+  })
+
 }
 
 function getJokes(req, res) {
